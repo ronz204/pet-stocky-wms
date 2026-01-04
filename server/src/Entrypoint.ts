8d629754bea2bedb93313f723b8ff0b799c6ea95
roney ruiz <roneyruizrojas@gmail.com>
2026-01-03 22:47:26 -0600

    feat: create simple express app

---
+++ server/src/Entrypoint.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+const expr = express();
+
+expr.get("/", (req, res) => {
+  res.status(200).json({ message: "Hello, World!" });
+});
+
+expr.listen(3000, () => {
+  console.log("Server is running on http://localhost:3000");
+});
