73270ac3aa3ee649bfc01b7338c57dad7de3480f
roney ruiz <roneyruizrojas@gmail.com>
2026-01-07 22:14:34 -0600

    feat: create apollo client example

--- client/src/Features/Playground/Page.tsx
+++ client/src/Features/Playground/Page.tsx
@@ -1,5 +1,22 @@
 import type { JSX } from "react";
+import { gql } from "@apollo/client";
+import { useQuery } from "@apollo/client/react";
+
+interface HelloQueryData {
+  hello: string;
+};
+
+const HELLO_QUERY = gql`
+  query Hello {
+    hello
+  }
+`;
 
 export const PlaygroundPage = (): JSX.Element => {
-  return <div className="font-bold underline">Playground Page</div>;
+  const { data, loading, error } = useQuery<HelloQueryData>(HELLO_QUERY);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
+
+  return <div className="font-bold underline">{data?.hello}</div>;
 };
