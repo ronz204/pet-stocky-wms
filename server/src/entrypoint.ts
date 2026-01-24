77d1ae90efb0e7889163394ce481c8fe31dadffd
roney ruiz <roneyruizrojas@gmail.com>
2026-01-24 00:38:42 -0600

    refac: use method chainning in entrypoint

--- server/src/entrypoint.ts
+++ server/src/entrypoint.ts
@@ -1,14 +1,6 @@
 import { yoga } from "@elysiajs/graphql-yoga";
 import { cors } from "@elysiajs/cors";
-import { Elysia } from "elysia";
-
-const app = new Elysia();
-
-app.use(cors({ origin: "*" }));
-
-app.get("/", () => {
-  return { message: "Hello Elysia" };
-});
+import { Elysia, t } from "elysia";
 
 const schema = /* GraphQL */`
   type Query {
@@ -16,16 +8,23 @@ const schema = /* GraphQL */`
   }
 `;
 
-app.use(yoga({
-  typeDefs: schema,
-  resolvers: {
-    Query: {
-      hello: () => "Hello from GraphQL Yoga with Elysia!"
-    }
-  },
-}));
-
-app.listen(3000);
+const app = new Elysia()
+  .env(t.Object({
+    PORT: t.Number(),
+  }))
+  .use(cors({ origin: "*" }))
+  .get("/", () => {
+    return { message: "Hello Elysia" };
+  })
+  .use(yoga({
+    typeDefs: schema,
+    resolvers: {
+      Query: {
+        hello: () => "Hello from GraphQL Yoga with Elysia!"
+      }
+    },
+  }))
+  .listen(process.env.PORT!);
 
 const url = `http://${app.server?.hostname}:${app.server?.port}`;
 console.log(`🦊 Elysia is running at ${url}`);
