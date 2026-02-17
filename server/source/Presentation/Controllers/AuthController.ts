a01526660e6ee377f232f79df65f489fd8518949
roney ruiz <roneyruizrojas@gmail.com>
2026-02-16 22:02:28 -0600

    feat(elysia): add validation to login endpoint

--- server/source/Presentation/Controllers/AuthController.ts
+++ server/source/Presentation/Controllers/AuthController.ts
@@ -1,9 +1,18 @@
-import { Elysia, type ElysiaConfig } from "elysia";
+import { Elysia, type ElysiaConfig, t } from "elysia";
 
 const config: ElysiaConfig<"/auth"> = {
   prefix: "/auth", name: "auth-controller",
 };
 
 export const AuthController = new Elysia(config)
-  .post("/login", async () => ({ message: "Login successful" }))
-  .post("/register", async () => ({ message: "Registration successful" }));
+  .post("/login", async ({ body }) => {
+    console.log(body.email);
+    console.log(body.password);
+
+    return { message: "Login successful" };
+  }, {
+    body: t.Object({
+      email: t.String(),
+      password: t.String(),
+    }),
+  });
