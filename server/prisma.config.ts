9f94ba4b0248696a1565e5a7b647b25369732d41
roney ruiz <roneyruizrojas@gmail.com>
2026-03-10 11:06:57 -0600

    feat(server): add good named models and migrations

--- server/prisma.config.ts
+++ server/prisma.config.ts
@@ -3,7 +3,7 @@ import { defineConfig, env } from "prisma/config";
 export default defineConfig({
   schema: "prisma/",
   migrations: {
-    path: "prisma/Migrations",
+    path: "prisma/migrations",
   },
   datasource: {
     url: env("POSTGRES_URL"),
