f5433f23ec598f7afbe575394f11eb6a7a710824
roney ruiz <roneyruizrojas@gmail.com>
2026-03-22 21:49:48 -0600

    feat(server): create prisma plugin

---
+++ server/source/database/prisma.plugin.ts
@@ -0,0 +1,9 @@
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@prisma/client";
+import { Elysia } from "elysia";
+
+const url = process.env.DATABASE_URL ?? "";
+const adapter = new PrismaPg({ connectionString: url });
+
+export const PrismaPlugin = new Elysia({ name: "prisma" })
+  .decorate("prisma", new PrismaClient({ adapter }));
