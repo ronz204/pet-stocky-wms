2cef81948e51560b0b5e0495010521e2f276f96a
roney ruiz <roneyruizrojas@gmail.com>
2026-03-23 20:28:54 -0600

    feat(server): add base inventory plugin

--- server/source/entry.ts
+++ server/source/entry.ts
@@ -1,8 +1,10 @@
 import { Elysia } from "elysia";
 import { HealthPlugin } from "@features/health/health.plugin";
+import { InventoryPlugin } from "@features/inventory/inventory.plugin";
 
 const app = new Elysia({ prefix: "/api" })
   .use(HealthPlugin)
+  .use(InventoryPlugin)
   .listen(process.env.PORT!);
 
 const url = `http://${app.server?.hostname}:${app.server?.port}`;
---
+++ server/source/features/inventory/inventory.plugin.ts
@@ -0,0 +1,3 @@
+import { Elysia } from "elysia";
+
+export const InventoryPlugin = new Elysia({ name: "inventory.plugin" });
