84c6881fbf987a4f45aa090561ad491d73bb0db9
roney ruiz <roneyruizrojas@gmail.com>
2026-03-25 21:56:31 -0600

    refac(server): simplify and rename read stock schemas

---
+++ server/source/features/inventory/modules/stock/schemas/read.schema.ts
@@ -0,0 +1,32 @@
+import { t, type Static } from "elysia";
+import { StockStatus } from "@prisma/enums";
+
+const StockDTO = t.Object({
+  sku: t.String(),
+  name: t.String(),
+  location: t.String(),
+  warehouse: t.String(),
+  stocklotId: t.Number(),
+  quantity: t.Number(),
+  status: t.Enum(StockStatus),
+});
+
+export const ReadStockParams = t.Object({
+  productId: t.Optional(t.Number()),
+  warehouseId: t.Optional(t.Number()),
+  locationCode: t.Optional(t.String()),
+});
+
+export const ReadStockRequest = t.Object({
+  params: ReadStockParams,
+});
+
+export const ReadStockResponse = t.Object({
+  totalItems: t.Number(),
+  totalQuantity: t.Number(),
+  items: t.Array(StockDTO),
+});
+
+export type ReadStockParams = Static<typeof ReadStockParams>;
+export type ReadStockRequest = Static<typeof ReadStockRequest>;
+export type ReadStockResponse = Static<typeof ReadStockResponse>;
