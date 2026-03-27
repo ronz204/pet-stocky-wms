3ffaa30b74dd8bccfea31cdc3a5610615d55f97a
roney ruiz <roneyruizrojas@gmail.com>
2026-03-26 21:42:20 -0600

    fix(prisma): set postgres url for client correctly

--- server/source/database/prisma.plugin.ts
+++ server/source/database/prisma.plugin.ts
@@ -2,7 +2,7 @@ import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
 import { Elysia } from "elysia";
 
-const url = process.env.DATABASE_URL ?? "";
+const url = process.env.POSTGRES_URL ?? "";
 const adapter = new PrismaPg({ connectionString: url });
 
 export const PrismaPlugin = new Elysia({ name: "prisma" })
