d07a4e3cbba8cbab26e0754eb4a2d09e86842e5d
roney ruiz <roneyruizrojas@gmail.com>
2026-03-26 21:41:41 -0600

    feat(server): create a simple response mapper

--- server/source/features/inventory/modules/stock/handlers/read.handler.ts
+++ server/source/features/inventory/modules/stock/handlers/read.handler.ts
@@ -9,7 +9,7 @@ import type { ReadStockResponse } from "../schemas/read.schema";
 export class ReadStockHandler implements Handler<ReadStockRequest, ReadStockResponse> {
   constructor(private prisma: PrismaClient) {};
 
-  public async handle(request: ReadStockRequest): Promise<any> {
+  public async handle(request: ReadStockRequest): Promise<ReadStockResponse> {
     const statsQuery = new StatsStockSpecify(request).toQuery();
     const readQuery = new ReadStockSpecify(request).toQuery();
 
@@ -17,5 +17,19 @@ export class ReadStockHandler implements Handler<ReadStockRequest, ReadStockResp
       this.prisma.stock.aggregate(statsQuery),
       this.prisma.stock.findMany(readQuery),
     ]);
+
+    return {
+      totalItems: stats._count.id,
+      totalQuantity: stats._sum.quantity?.toNumber() ?? 0,
+      items: data.map((item) => ({
+        sku: item.product.sku,
+        name: item.product.name,
+        quantity: item.quantity.toNumber(),
+        status: item.status,
+        warehouse: item.location.werehouse.name,
+        location: item.location.code,
+        stocklotId: item.id,
+      })),
+    };
   };
 };
