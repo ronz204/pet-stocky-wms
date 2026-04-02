feced91f280f227ac44d67203abab139eacd5e84
roney ruiz <roneyruizrojas@gmail.com>
2026-04-02 15:16:15 -0600

    chore(client): purge react client project

--- client/src/main.tsx
+++
@@ -1,7 +0,0 @@
-import "./index.css";
-import App from "./App.tsx";
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-
-const root = document.getElementById("root")!;
-createRoot(root).render(<StrictMode><App /></StrictMode>);
